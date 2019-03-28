a5fe765d7ac2c2ed547c5db336a69bb8ba9d17f7
shge <25113178+shge@users.noreply.github.com>
2019-03-29 02:53:21 +0900

    Add IP address

--- index.py
+++ index.py
@@ -9,26 +9,27 @@ line_bot_api = LineBotApi(os.environ['ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def redirect_page():
-    return redirect("https://github.com/shge/line-bot", code=303)
+    print('Redirected: ' + request.remote_addr)
+    return redirect('https://github.com/shge/line-bot', code=303)
 
 
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def callback():
     try:
         signature = request.headers['X-Line-Signature']
     except KeyError:
-        print("No signature in header.")
+        print('No signature in header:' + request.remote_addr)
         abort(400)
 
     body = request.get_data(as_text=True)
-    print("Request body: " + body)
+    print('Request body: ' + body)
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Signature was in header, but invalid.")
+        print('Signature was in header, but invalid:' + request.remote_addr)
         abort(400)
 
     return 'OK'
@@ -38,7 +39,7 @@ def callback():
 def handle_message(event):
 
     received = event.message.text
-    send_message = received + "！"
+    send_message = received + '！'
 
     line_bot_api.reply_message(
         event.reply_token,
@@ -46,15 +47,15 @@ def handle_message(event):
     )
 
 
-# if __name__ == "__main__":
+# if __name__ == '__main__':
 #     app.run()
-    # port = int(os.getenv("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
+    # port = int(os.getenv('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
 
 # from http.server import BaseHTTPRequestHandler
 #
-# PUSH_URL = "https://api.line.me/v2/bot/message/push"
-# REPLY_URL = "https://api.line.me/v2/bot/message/reply"
+# PUSH_URL = 'https://api.line.me/v2/bot/message/push'
+# REPLY_URL = 'https://api.line.me/v2/bot/message/reply'
 #
 #
 # class handler(BaseHTTPRequestHandler):
