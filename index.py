12a9f7ea17fd1af938d5718f73f1213853f49626
shge <25113178+shge@users.noreply.github.com>
2019-03-28 16:16:38 +0900

    Use Now.sh again

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ line_bot_api = LineBotApi(os.environ['ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 
-@app.route("/", methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
