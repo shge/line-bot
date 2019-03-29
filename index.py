187964d886b668f0829f8f5bea2f8d49081cabdc
shge <25113178+shge@users.noreply.github.com>
2019-03-29 10:47:20 +0900

    Add "profile"

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 import os
+import sys
 from flask import Flask, request, abort, redirect
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookHandler, SourceUser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
-line_bot_api = LineBotApi(os.environ['ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+access_token = os.getenv('ACCESS_TOKEN', None)
+channel_secret = os.getenv('CHANNEL_SECRET', None)
+if access_token is None or channel_secret is None:
+    print('Specify ACCESS_TOKEN and CHANNEL_SECRET as environment variables.')
+    sys.exit(1)
+line_bot_api = LineBotApi(access_token)
+handler = WebhookHandler(channel_secret)
 
 
 @app.route('/', methods=['GET'])
@@ -43,41 +49,26 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    received = event.message.text
-    send_message = received + '！'
+    text = event.message.text
+
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text='Display name: ' + profile.display_name),
+                    TextSendMessage(text='Status message: ' + profile.status_message)
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Bot can't use profile API without user ID"))
+
+    # Procsss the message here
+    reply = text + '！'
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=send_message)
+        TextSendMessage(text=reply)
     )
-
-
-# if __name__ == '__main__':
-#     app.run()
-    # port = int(os.getenv('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-
-# from http.server import BaseHTTPRequestHandler
-#
-# PUSH_URL = 'https://api.line.me/v2/bot/message/push'
-# REPLY_URL = 'https://api.line.me/v2/bot/message/reply'
-#
-#
-# class handler(BaseHTTPRequestHandler):
-#
-#     def do_GET(self):
-#         self.send_response(303)
-#         self.send_header('Location', 'https://github.com/shge/line-bot')
-#         # self.send_header('Content-type','text/plain')
-#         self.end_headers()
-#         # message = 'Hello!'
-#         # self.wfile.write(message.encode())
-#         return
-#
-#     def do_POST(self):
-#         self.send_response(200)
-#         # self.send_header('Content-type','text/plain')
-#         self.end_headers()
-#         # message = 'Hello!'
-#         # self.wfile.write(message.encode())
-#         return
