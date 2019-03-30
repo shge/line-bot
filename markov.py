3b6d01389065cafce644e2fc17094ed637c25a6c
shge <25113178+shge@users.noreply.github.com>
2019-03-30 15:57:34 +0900

    Add make_short_sentences_with_start

--- markov.py
+++ markov.py
@@ -8,9 +8,21 @@ fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
 logging.basicConfig(level=logging.DEBUG, format=fmt)
 
 
+def make_short_sentences(text, max=300, min=0, tries=100):
+    for _ in range(tries):
+        sentence = text.make_sentence().replace(' ', '')
+        if sentence and len(sentence) <= max and len(sentence) >= min:
+            return sentence
+
+def make_short_sentences_with_start(text, beginning, max=300, min=0, tries=100):
+    for _ in range(tries):
+        sentence = text.make_sentence_with_start(beginning).replace(' ', '')
+        if sentence and len(sentence) <= max and len(sentence) >= min:
+            return sentence
+
+
 # Load file
-# text_file = open("input.txt", "r")
-# text = text_file.read()
+# text = open("input.txt", "r").read()
 # logger.info('File loaded')
 
 
@@ -19,7 +31,7 @@ parsed_text = ''
 for line in open('input.txt', 'r'):
     parsed_text += MeCab.Tagger('-Owakati').parse(line)
 parsed_text = parsed_text.rstrip('\n')
-parsed_text = re.sub('。(.)', '。\n\1', parsed_text)
+parsed_text = re.sub('[。．](.)', '。\n\1', parsed_text)
 # logger.info('Text parsed')
 
 
@@ -30,6 +42,7 @@ logger.info('Text model built')
 
 
 # Output (max, min)
-# for _ in range(10):
-sentence = text_model.make_short_sentence(100, 30, tries=100).replace(' ', '')
-logger.info(sentence)
+for _ in range(10):
+    # sentence = make_short_sentences(text_model, max=100, min=50)
+    sentence = make_short_sentences_with_start(text_model, 'メロス', max=30, min=5)
+    logger.info(sentence)
